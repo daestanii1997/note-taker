6535eea0ed8f65587ee0c7b7984a392c84ba2609
Dae <daestanii1997@gmail.com>
2023-09-12 17:09:12 -0600

    added notes to finish project

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,7 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// should read db and return notes as json
 app.get('/api/notes', (req, res) =>
     res.json(db)
 );
@@ -22,7 +23,30 @@ app.get('*', (req, res) =>
 );
 
 app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+    // should receive new note to save on req body add to db file and return new note to client.
+    // give each note a unique id when its saved(look at npm packages to automatically do this)
+    
+    const { noteTitle, noteText } = req.body;
+
+    if (noteTitle && noteText) {
+        const newNote = {
+            noteTitle,
+            noteText,
+        };
+        // read file then...
+        // convert response to json format to add to db file
+        db.push(newNote)
+        // then convert back
+        // writefile
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+    }
+
     console.log(response);
+    res.status(201).json(response);
 })
 
 app.listen(PORT, () =>
