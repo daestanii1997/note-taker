643af39122c71cc85d316bc28b07169c6404dcf4
Dae <daestanii1997@gmail.com>
2023-09-14 18:32:17 -0600

    worked on post request

--- Develop/server.js
+++ Develop/server.js
@@ -1,53 +1,79 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// const uuid = require('./helpers/uuid');
 // const api = require('./public/assets/js/index.js');
-const db = require('./db/db.json');
+const noteRecord = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// should read db and return notes as json
-app.get('/api/notes', (req, res) =>
-    res.json(db)
+app.get("/api/notes", (req, res) => res.json(noteRecord));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a review`);
+  // should receive new note to save on req body add to db file and return new note to client.
+  // give each note a unique id when its saved(look at npm packages to automatically do this)
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a review`);
-    // should receive new note to save on req body add to db file and return new note to client.
-    // give each note a unique id when its saved(look at npm packages to automatically do this)
-    
-    const { noteTitle, noteText } = req.body;
+  const { noteTitle, noteText } = req.body;
 
-    if (noteTitle && noteText) {
-        const newNote = {
-            noteTitle,
-            noteText,
-        };
-        // read file then...
+  if (noteTitle && noteText) {
+    const newNote = {
+      noteTitle,
+      noteText,
+    //   note_id: uuid(),
+    };
+
+    // read file then...
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        // return;
+      } else {
+
         // convert response to json format to add to db file
-        db.push(newNote)
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNote);
+
         // then convert back
         // writefile
-        const response = {
-            status: 'success',
-            body: newNote,
-          };
-    }
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("note updated!");
+          }
+        });
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
 
     console.log(response);
     res.status(201).json(response);
-})
+
+  } else {
+    res.status(500).json("Error in posting review");
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
